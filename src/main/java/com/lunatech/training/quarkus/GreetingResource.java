cd3dbe5dbb9c0ab147edd416a85eb53bf38ae4e5
Erik Bakker <erik@eamelink.net>
2021-03-01 17:11:38 +0100

    Solution of Exercise 6

--- src/main/java/com/lunatech/training/quarkus/GreetingResource.java
+++ src/main/java/com/lunatech/training/quarkus/GreetingResource.java
@@ -2,7 +2,10 @@ package com.lunatech.training.quarkus;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import io.quarkus.runtime.StartupEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,14 +17,25 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.TEXT_PLAIN)
 public class GreetingResource {
 
+    private final SubjectBean subjectBean;
+    private final String greeting;
+    private final Template greet;
+
+    public GreetingResource(SubjectBean subjectBean, @ConfigProperty(name = "greeting") String greeting, Template greet) {
+        this.subjectBean = subjectBean;
+        this.greeting = greeting;
+        this.greet = greet;
+    }
+
+    void startup(@Observes StartupEvent event) {
+        System.out.println("GreetingResource Ready!");
+    }
+
     @GET
     @Path("greet")
     public String greet() {
-        return "Hello, Quarkians!";
+        return greeting + ", " + subjectBean.subject();
     }
-
-    @Inject
-    Template greet;
 
     @GET
     @Path("hello/{subject}")
--- src/test/java/com/lunatech/training/quarkus/GreetingResourceTest.java
+++ src/test/java/com/lunatech/training/quarkus/GreetingResourceTest.java
@@ -1,7 +1,10 @@
 package com.lunatech.training.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -9,13 +12,17 @@ import static org.hamcrest.CoreMatchers.containsString;
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @Inject
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
     @Test
     public void testGreetEndpoint() {
         given()
           .when().get("/greet")
           .then()
              .statusCode(200)
-             .body(containsString("Hello"));
+             .body(containsString(greeting));
     }
 
 }
