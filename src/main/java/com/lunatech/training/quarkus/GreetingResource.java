ab890c1964e137928e69c32e947879206bbf920c
Erik Bakker <erik@eamelink.net>
2021-03-01 13:32:08 +0100

    Solution of Exercise 1

--- src/main/java/com/lunatech/training/quarkus/GreetingResource.java
+++ src/main/java/com/lunatech/training/quarkus/GreetingResource.java
@@ -2,6 +2,7 @@ package com.lunatech.training.quarkus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,6 +14,12 @@ public class GreetingResource {
     @Path("greet")
     public String greet() {
         return "Hello, Quarkians!";
+    }
+
+    @GET
+    @Path("hello/{subject}")
+    public String hello(@PathParam("subject") String subject) {
+        return "Hello, " + subject;
     }
 
 }
