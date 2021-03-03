37e1eef744925f2e2e9d93a8dbd1ce621f65fc27
Erik Bakker <erik@eamelink.net>
2021-03-03 10:21:47 +0100

    Solution of Exercise 10

--- src/main/java/com/lunatech/training/quarkus/Product.java
+++ src/main/java/com/lunatech/training/quarkus/Product.java
@@ -1,15 +1,28 @@
 package com.lunatech.training.quarkus;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
 public class Product extends PanacheEntity {
 
+    @NotNull
+    @Length(min = 3)
     public String name;
+
+    @NotNull
+    @Length(min = 10)
     public String description;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = true)
+    @Digits(integer = 10, fraction = 2)
     public BigDecimal price;
 
     public Product(){}
